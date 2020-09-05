3ee443e314cbdd476bc7014148f692c677e05740
Saulo <saulo.guedes.jr@gmail.com>
2020-09-05 16:02:53 -0300

    Homepage: adding images to card-menu

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'marvel-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('cardAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
+        animate('600ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+
+  public cardState = 'ready'
 
   constructor() { }
 
