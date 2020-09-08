eb5f1c8c1072d0001f44d5faff31bef9a6984e74
Saulo <saulo.guedes.jr@gmail.com>
2020-09-08 18:07:47 -0300

    Generating build/docs folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  marvelApiUrl: "https://gateway.marvel.com:443/v1/public/",
+  marvelApiKey: "bb35bb1f3c3daecff07529468c507462"
 };
